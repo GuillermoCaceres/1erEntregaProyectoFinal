2ccd0a594c6666d979aff06307959e69798d5054
Guillermo Caceres <guillermohcaceres@gmail.com>
2022-08-09 22:46:14 -0300

    Desafio 2da entrega

--- carrito.js
+++ carrito.js
@@ -1,43 +1,19 @@
-class Prod {
-    constructor(id, prod, precio, stock) {
-        this.id = id;
-        this.prod = prod;
-        this.precio = precio;
-        this.stock = stock;
-    }
+const productos = [ 
+    {id: 1, prod:"Ambo1", precio: 3799, stock:3},
+    {id: 2, prod:"Ambo2", precio: 4499, stock:5},
+    {id: 3, prod:"Ambo3", precio: 3499, stock:9},
+    {id: 4, prod:"Ambo4", precio: 3899, stock:12}
+];
+const guardarLocal = (clave,valor) => { localStorage.setItem(clave,valor)};
+//almacenar
+for (const producto of productos) {
+    guardarLocal("listaProductos",JSON.stringify(productos))
 }
-const productos = [];
-productos.push(new Prod(1, "Ambo1" , 3799, 3));
-productos.push(new Prod(2, "Ambo2" , 4499, 5));
-productos.push(new Prod(3, "Ambo3" , 3499, 9));
-productos.push(new Prod(4, "Ambo4" , 3899, 12));
-/* productos.push(new Prod(5, "Ambo5" , 3899, 4));
-productos.push(new Prod(6, "Ambo6" , 5499, 8));
-productos.push(new Prod(7, "Ambo7" , 5499, 6));
-productos.push(new Prod(8, "Ambo8" , 3499, 7)); */
-/* console.log(productos);
-
-function elemento(producto, id)
-    {
-        return productos.find(objeto => objeto.id === id);
-    }
-                        
-         */
-
-/*
-let titulo = document.getElementById("titulo");
-titulo.innerHTML = prompt("ingrese titulo nuevo")
-*/
-/* /*llamar a un elemento por clase
-const cabecera1 = document.getElementsByClassName("cabecera1")
-console.log(cabecera1)
-llamar un elemento por queryselector
-* para class
-# para id
-
-const prueba = document.querySelector(".conteiner1")
-console.log(prueba) 
-*/
+let carrito = []
+let carritoEnLS = JSON.stringify(localStorage.getItem('carrito'))
+if (carritoEnLS) {
+    carrito = carritoEnLS
+}
 let boton
 let total = 0
 boton = document.getElementById("Boton1")
@@ -49,6 +25,7 @@ boton.onclick = () =>
             console.log("el total es $"+ total)
             productos[0].stock = productos[0].stock - 1
             console.log(productos[0])
+            guardarLocal("listaProductos",JSON.stringify(productos[0]))
         }
         else 
         {
@@ -66,6 +43,7 @@ boton.onclick = () =>
             console.log("el total es $"+ total)
             productos[1].stock = productos[1].stock - 1
             console.log(productos[1])
+            guardarLocal("listaProductos",JSON.stringify(productos[1]))
         }
         else 
         {
@@ -82,7 +60,9 @@ boton.onclick = () =>
             total = total + productos[2].precio
             console.log("el total es $"+ total)
             productos[2].stock = productos[2].stock - 1
-            console.log(productos[2])}
+            console.log(productos[2])
+            guardarLocal("listaProductos",JSON.stringify(productos[2]))
+        }
         else 
         {
             let sinStock = document.getElementById("Boton3");
@@ -99,6 +79,7 @@ boton.onclick = () =>
             console.log("el total es $"+ total)
             productos[3].stock = productos[3].stock - 1
             console.log(productos[3])
+            guardarLocal("listaProductos",JSON.stringify(productos[3]))
         }
         else 
         {
